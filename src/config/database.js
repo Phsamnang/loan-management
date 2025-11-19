2611ca5a84c94bfc663754c01248914b5d345cf3
somnang <somnang.dev@gmail.com>
2025-11-19 13:31:24 +0700

    modify some endpoint

--- src/config/database.js
+++ src/config/database.js
@@ -4,9 +4,6 @@ dotenv.config();  // <-- MUST be first
 const DATABASE_URL =
   process.env.DATABASE_URL;
 
-
-  console.log("Database URL:", DATABASE_URL);
-
 if (!DATABASE_URL) {
   throw new Error("DATABASE_URL environment variable is not defined");
 }
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,8 +1,10 @@
 import express from "express";
 const router = express.Router();
 import AuthController from "../controllers/authController.js";
+import { ro } from "date-fns/locale";
 
-router.post("/register", AuthController.register);  
+router.post("/register", AuthController.register);
+router.post("/login", AuthController.login);
 
 
 export default router;
